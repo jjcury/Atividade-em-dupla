18a1fbf0b35b989c633fb58fed876502ce430f1f
Joaquim Felipe Cury <joaquim.04293@aluno.iffar.edu.br>
2026-06-02 17:18:54 -0300

    Corrigi a questao 1

--- questoes/1.py
+++ questoes/1.py
@@ -1,34 +1,35 @@
 class CapsulaDoTempo:
     def __init__(self, autor, mensagem, ano_abertura, ano_atual):
-        self.autor=autor
-        self.mensagem=mensagem
-        self.ano_abertura=ano_abertura
-        self.ano_atual=ano_atual
-    
+        self.autor = autor
+        self.mensagem = mensagem
+        self.ano_abertura = ano_abertura
+        self.ano_atual = ano_atual
+
     def pode_abrir(self):
-        if self.ano_atual==self.ano_abertura:
+        if self.ano_atual >= self.ano_abertura:
             print("A cápsula pode ser aberta!")
         else:
             print("Você ainda não pode abrir a cápsula.")
-    
+
     def calcular_espera(self):
-        return(f"Faltam {self.ano_abertura-self.ano_atual} anos para a cápsula ser aberta.")
+        return self.ano_abertura - self.ano_atual
 
     def classificar_espera(self):
-        TempoDeEspera=self.pode_abrir()
-        if TempoDeEspera==0:
+        tempo_de_espera = self.calcular_espera()
+
+        if tempo_de_espera <= 0:
             print("Pode abrir agora.")
-        
-        elif TempoDeEspera>=1 and TempoDeEspera<=3:
+        elif tempo_de_espera <= 3:
             print("Espera curta.")
-        
-        else: print("Espera longa.")
-    
+        else:
+            print("Espera longa.")
+
     def exibir_resumo(self):
-        print(self.autor)
-        print(self.ano_abertura)
+        print("Autor:", self.autor)
+        print("Ano de abertura:", self.ano_abertura)
         self.classificar_espera()
-        print(self.mensagem)
-    
+        print("Mensagem:", self.mensagem)
+
+
 cap1 = CapsulaDoTempo("Joaquim", "oi", 2030, 2026)
-cap1.exibir_resumo
+cap1.exibir_resumo()
